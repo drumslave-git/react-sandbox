876b24de8ff9d130d98a6826de0c1f0a13ee0798
KleoPetroff <kleopetroff@gmail.com>
2016-03-10 00:57:40 +0200

    convert app component to stateless functional component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const App = React.createClass({
-    render() {
-        return (
-            <div>
-                <h2>Hello World</h2>
-            </div>
-        )
-    }
-});
+const App = function () {
+    return (
+        <div>
+            <h2>Hello ReactJS</h2>
+        </div>
+    )
+};
 
 export default App;
