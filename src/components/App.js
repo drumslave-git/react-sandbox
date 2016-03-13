77a87915a8b2dae4038464ae15f8e4be63c86156
KleoPetroff <kleopetroff@gmail.com>
2016-03-13 17:55:55 +0200

    change  stateless functional component to regular react component
    hot-react-loader reloads the whole page if stateless functional component is use. Reverting the component to a regular react.createclass component fix the problem

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = React.createClass({
+    render() {
+        return (
+            <div>
+                <h2>Hello ReactJS</h2>
+            </div>
+        )
+    }
+}) ;
+
+export default App;
