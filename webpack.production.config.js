b0eb8dfca9a55efa8e597662ae20e751e30a61bd
KleoPetroff <kleopetroff@gmail.com>
2016-03-21 21:54:52 +0200

    separate development and production webpack configs

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,8 +1,8 @@
 var path = require('path');
-
 var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/src/index.html',
   filename: 'index.html',
@@ -10,22 +10,22 @@ var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 });
 
 var config = {
-  devtool: 'source-map',
+  devtool: 'cheap-module-source-map',
 
   entry: path.resolve(__dirname, 'src/main.js'),
 
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'dist')
   },
 
   module: {
     loaders: [
       {
         test: /\.js?$/,
-        exclude: /node_modules/,
         include: path.resolve(__dirname, 'src'),
-        loaders: ['react-hot', 'babel']
+        exclude: /node_modules/,
+        loader: 'babel'
       },
       {
         test: /\.scss$/,
@@ -39,12 +39,15 @@ var config = {
     ]
   },
 
-  plugins: [
-    HtmlWebpackPluginConfig,
+  plugins: [HtmlWebpackPluginConfig,
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({comments: false}),
+    new webpack.DefinePlugin({
+      'process.env': {NODE_ENV: JSON.stringify('production')}
+    }),
     new ExtractTextPlugin('styles/style.css', {
       allChunks: true
-    })
-  ]
+    })]
 };
 
 module.exports = config;
