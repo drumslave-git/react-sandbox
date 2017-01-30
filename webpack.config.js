9e4632d7df71107f5ea52283c062415c86a2da20
KleoPetroff <kleopetroff@gmail.com>
2017-01-31 01:17:40 +0200

    add eot, woff, woff2, otf and svg transilation

--- webpack.config.js
+++ webpack.config.js
@@ -55,11 +55,12 @@ const config = {
           ],
         }),
       },
-      {
-        test: /\.(png|jpg)$/,
-        loader: 'url?limit=15000',
-      },
-    ],
+      { test: /\.(png|jpg)$/, use: 'url-loader?limit=15000' },
+      { test: /\.eot(\?v=\d+.\d+.\d+)?$/, use: 'file-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml' },
+    ]
   },
 
   plugins: [
