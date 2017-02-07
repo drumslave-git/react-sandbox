3876f692f8ff9ddc8e62575cfe632b90604b76d4
KleoPetroff <kleopetroff@gmail.com>
2017-02-07 09:44:17 +0200

    change react-router and history versions
    handled queryKey error on initialization

--- app/config/Root.js
+++ app/config/Root.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import { Router, useRouterHistory } from 'react-router';
-import createHashHistory from 'react-router/node_modules/history/lib/createHashHistory';
-import routes from './routes';
+import { Router, Route, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
+
+import App from '../components/App';
+
 
 const history = useRouterHistory(createHashHistory)({ queryKey: false });
 
 export default class Root extends Component {
   render() {
     return (
-      <Router history={history} key={new Date()} routes={routes} />
+      <Router history={history} key={new Date()}>
+        <Route path="/" component={App} />
+      </Router>
     );
   }
 }
