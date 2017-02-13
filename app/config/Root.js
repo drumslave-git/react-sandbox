2305bc33f93036b8e0fc17c27c7752a44729d7ec
KleoPetroff <kleopetroff@gmail.com>
2017-02-14 00:16:58 +0200

    use shortid as key Router value instead of Date

--- app/config/Root.js
+++ app/config/Root.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Router, Route, useRouterHistory } from 'react-router';
 import { createHashHistory } from 'history';
+import shortid from 'shortid';
 
 import App from '../components/App';
 
@@ -10,7 +11,7 @@ const history = useRouterHistory(createHashHistory)({ queryKey: false });
 export default class Root extends Component {
   render() {
     return (
-      <Router history={history} key={new Date()}>
+      <Router history={history} key={shortid.generate()}>
         <Route path="/" component={App} />
       </Router>
     );
