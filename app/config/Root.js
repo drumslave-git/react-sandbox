834cddd56ec7ebfee338542bda2bea36bb70cb28
KleoPetroff <kleopetroff@gmail.com>
2017-06-29 01:20:28 +0300

    convert Root to stateless component

--- app/config/Root.js
+++ app/config/Root.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from '../components/App';
 
-export default class Root extends Component {
-  render() {
-    return (
-      <Router>
-        <Route path="/" component={App} />
-      </Router>
-    );
-  }
-}
+const Root = () => {
+  return (
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
+  );
+};
+
+export default Root;
 
