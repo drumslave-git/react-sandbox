2a7f474be5b11bf89bf8fd37c9a48e6de3bb2cb7
KleoPetroff <kleopetroff@gmail.com>
2017-10-16 21:41:15 +0300

    migrate project to react 16

--- tests/components/App.test.js
+++ tests/components/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import App from '../../app/components/App';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('App Component', () => {
   let wrapper;
