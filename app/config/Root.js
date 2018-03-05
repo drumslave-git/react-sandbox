b6c0d1e08c1aeef0004ee1c55b78305ca4e0e979
KleoPetroff <kleopetroff@gmail.com>
2018-03-05 22:32:27 +0200

    wrap the routes in switch component

--- app/config/Root.js
+++ app/config/Root.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from '../components/App';
 
 const Root = () => {
   return (
     <Router>
-      <Route path="/" component={App} />
+      <Switch>
+        <Route path="/" component={App} exact />
+      </Switch>
     </Router>
   );
 };
