9199f3124118f68ef70d3be19493907a220896de
drumslave <george.tislenko@gmail.com>
2018-12-17 23:32:46 +0200

    fix path again x 2

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 9090);
 
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 var server = app.listen(app.get('port'), () => {
     console.log('listening on port ', server.address().port);
 });
